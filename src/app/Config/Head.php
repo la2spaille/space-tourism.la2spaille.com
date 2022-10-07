20c81f3d69cc51bd90093856d025bdc9daacacd6
Wilfried <la2spaille@gmail.com>
2022-10-07 20:37:43 +0100

    hosting

--- src/app/Config/Head.php
+++ src/app/Config/Head.php
@@ -5,8 +5,8 @@ namespace App\Config;
 class Head {
 
     public static function data () {
-        $head['urlBase'] = 'https://space-tourism-by-la2spaille.herokuapp.com'; // Desktop version only with protocol
-        $head['serverName'] = 'space-tourism-by-la2spaille.herokuapp.com'; // Desktop or mobile without protocol
+        $head['urlBase'] = 'https://spacetourism.myhostme.space/'; // Desktop version only with protocol
+        $head['serverName'] = 'spacetourism.myhostme.space'; // Desktop or mobile without protocol
 
         $head['twitter']['pseudo']  = '@la2spaille';
         $head['twitter']['creator'] = '@la2spaille';
