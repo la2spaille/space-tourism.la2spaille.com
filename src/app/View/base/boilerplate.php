3f8f7034447094d59a1a89ec29aba0c8b013d6a2
Wilfried <la2spaille@gmail.com>
2022-10-07 20:48:22 +0100

    Update boilerplate.php

--- src/app/View/base/boilerplate.php
+++ src/app/View/base/boilerplate.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <link rel="canonical" href="<?= $this->head['url']; ?>">
     <!-- Robots -->
