41f89db9571a3bf684fb130a51a7b7f86ec17ecd
Wilfried <la2spaille@gmail.com>
2022-10-07 20:26:13 +0100

    Update Autoloader.php

--- src/engine/Core/Autoloader.php
+++ src/engine/Core/Autoloader.php
@@ -15,9 +15,13 @@ class Autoloader
         $rootFolder = explode('\\', $class)[0];
 
         if ($rootFolder === 'Engine' || $rootFolder === 'App') {
+            echo $class;
+            echo '<br>';
             $class = str_replace($rootFolder, '', $class);
             $class = str_replace('\\', '/', $class);
             echo ROOT . strtolower($rootFolder) . $class . '.php';
+            echo '<br>';
+
             include ROOT . strtolower($rootFolder) . $class . '.php';
         }
     }
